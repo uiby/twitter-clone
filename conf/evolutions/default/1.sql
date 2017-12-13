fd977946edd1ca80b06d96205a51f2b3ee795fdb
Kenya <win10.kesku@gmail.com>
2017-12-14 00:48:31 +0900

    add database table.

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,35 @@
+# --- First database schema
+
+# --- !Ups
+create table users (
+  user_id                        bigint not null auto_increment,
+  user_name                      varchar(255) not null,
+  email                          varchar(255) not null,
+  password                       varchar(8) not null,
+  PRIMARY KEY (user_id)
+);
+
+create table tweets (
+	tweet_id                        bigint not null auto_increment,
+	messages                        varchar(140) not null,
+  user_id                         bigint not null,
+  favorite_count                  int not null default 0,
+  retweet_count                   int not null default 0,
+  PRIMARY KEY (tweet_id)
+);
+
+create table favorites (
+	tweet_id                        bigint not null,
+	user_id                         bigint not null
+);
+
+create table relations (
+	user_id                         bigint not null,
+	follower_id                    bigint not null
+);
+
+# --- !Downs 
+drop table if exists users;
+drop table if exists tweets;
+drop table if exists favorites;
+drop table if exists relations;
